6ad3ee8dda4fd92e7b53bb3c2a8e0a69aa61aded
jessimeteo <jessica.laengle@uibk.ac.at>
2025-03-27 15:36:07 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-/*  L. steht für Leaflet, View([long, lat], zoomfaktor) */
+
+/* 
+Skript für die Neuseelandreise
+*/
+
+// Karte initialisieren
 var map = L.map('map').setView([-42.411667, 173.682222], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-        var marker = L.marker([-42.411667, 173.682222]).addTo(map);
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+// Hintergrundkarte definieren
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+//L. steht für Leaflet, View([long, lat], zoomfaktor)
+
+// Marker zeichnen
+var marker = L.marker([-42.411667, 173.682222]).addTo(map);
+
+// Popup definieren und öffnen
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
