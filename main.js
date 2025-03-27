47c071524b66bc8969fefd5dd9d436e2294fa02e
jessimeteo <jessica.laengle@uibk.ac.at>
2025-03-27 15:38:46 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ Skript für die Neuseelandreise
 */
 
 // Karte initialisieren
-var map = L.map('map').setView([-42.411667, 173.682222], 13);
+let map = L.map('map').setView([-42.411667, 173.682222], 13);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -14,7 +14,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //L. steht für Leaflet, View([long, lat], zoomfaktor)
 
 // Marker zeichnen
-var marker = L.marker([-42.411667, 173.682222]).addTo(map);
+let marker = L.marker([-42.411667, 173.682222]).addTo(map);
 
 // Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
