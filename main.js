c6de07204b75b938cf00127dede4ba7917a221ce
jessimeteo <jessica.laengle@uibk.ac.at>
2025-03-27 16:24:02 +0100

    Objekt for den Stopp definiert

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ let lat = -42.411667;
 let lng = 173.682222;
 let zoom = 11;
 
+let stop ={
+    nr: 29,
+    title: 'Kaikoura',
+    user: 'jessimeteo',
+    lat: lat,
+    lng: lng,
+    zoom: zoom
+}
+
 // Karte initialisieren
 let map = L.map('map').setView([lat, lng], zoom);
 
@@ -29,3 +38,18 @@ marker.bindPopup(`
         <li> Geogr. Länge: ${lng.toFixed(5)}° </li>
     </ul>.
 `).openPopup();
+
+// Objekt definieren
+let course = {
+    title: "Webmapping",
+    semester: '25S',
+    stunden: 3,
+    typ: 'VU'
+};
+
+console.log('title', course.title);
+console.log('semester', course.semester);
+console.log('stunden', course.stunden);
+console.log('typ', course.typ);
+
+console.log(stop.lat)
